f08fb0e9a38db78a7277b73e0f63ab6de9f9a06a
damian <adamodamian6@gmail.com>
2023-08-02 22:27:55 -0300

    put en diagnosticos

--- CMR Posta/src/main/java/com/posta/crm/controller/ProcessEmpresarioController.java
+++ CMR Posta/src/main/java/com/posta/crm/controller/ProcessEmpresarioController.java
@@ -6,7 +6,17 @@ package com.posta.crm.controller;
 
 import com.posta.crm.entity.Process;
 import com.posta.crm.entity.ProcessEmpresario;
+import com.posta.crm.entity.empresario.AnalisisResultados;
+import com.posta.crm.entity.empresario.ConceptosGenerales;
+import com.posta.crm.entity.empresario.Diagnostico;
+import com.posta.crm.entity.empresario.DiagnosticoEmpresarial;
+import com.posta.crm.repository.empresario.AnalisisResultadosRepository;
+import com.posta.crm.repository.empresario.ConceptosGeneralesRepository;
+import com.posta.crm.repository.empresario.DiagnosticoRepository;
 import com.posta.crm.service.empresario.ProcessEmpresarioServiceImpl;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -32,6 +42,12 @@ public class ProcessEmpresarioController {
     
     @Autowired
     private ProcessEmpresarioServiceImpl ProcessEmpresario;
+    @Autowired
+    DiagnosticoRepository diagnosticoRepository;
+    @Autowired
+    ConceptosGeneralesRepository conceptosGeneralesRepository;
+    @Autowired
+    AnalisisResultadosRepository analisisResultadosRepository;
     
     //Todos los POST y PUT para cada parte del Preceso empresario
     @PostMapping
@@ -39,13 +55,62 @@ public class ProcessEmpresarioController {
         
         return ResponseEntity.status(HttpStatus.CREATED).body(ProcessEmpresario.save(process));
     }
-    @PutMapping("/diagnostico")
-    public ResponseEntity<?>updateDiagnostico(@RequestBody Process process){
+    // el id que pido en este metodo es el del diagnostico
+    @PutMapping("/diagnostico/{id}")
+    public ResponseEntity<?>updateDiagnostico(@RequestBody Process process,@PathVariable Long id){
+        Diagnostico diagnostico = new Diagnostico();
+        Optional<Diagnostico> opcionDiagnostico = diagnosticoRepository.findById(id);
+        ConceptosGenerales conceptosGenerales = new ConceptosGenerales();
+        List<ConceptosGenerales> generalesList = new ArrayList<>();
+        if (opcionDiagnostico.isPresent()){
+           // ver el tema de conceptos generales
 
-        
-        return ResponseEntity.status(HttpStatus.CREATED).body(ProcessEmpresario.save(process));
+                process.getProcessEmpresario().getDiagnosticoEmpresarial().getDiagnostico().getConceptosGenerales().forEach(front->{
+
+                    generalesList.add(conceptosGeneralesRepository.save(front));
+
+                });
+
+
+            diagnostico.setConceptosGenerales(generalesList);
+            diagnostico.setGestionAmbiental(process.getProcessEmpresario().getDiagnosticoEmpresarial().getDiagnostico().getGestionAmbiental());
+            diagnostico.setGestionDigital(process.getProcessEmpresario().getDiagnosticoEmpresarial().getDiagnostico().getGestionDigital());
+            diagnostico.setGestionEstrategica(process.getProcessEmpresario().getDiagnosticoEmpresarial().getDiagnostico().getGestionEstrategica());
+            diagnostico.setGestionCalidad(process.getProcessEmpresario().getDiagnosticoEmpresarial().getDiagnostico().getGestionCalidad());
+            diagnostico.setGestionFinanciera(process.getProcessEmpresario().getDiagnosticoEmpresarial().getDiagnostico().getGestionFinanciera());
+            diagnostico.setGestionIntelectual(process.getProcessEmpresario().getDiagnosticoEmpresarial().getDiagnostico().getGestionIntelectual());
+            diagnostico.setGestionInnovacion(process.getProcessEmpresario().getDiagnosticoEmpresarial().getDiagnostico().getGestionInnovacion());
+            diagnostico.setGestionOperacional(process.getProcessEmpresario().getDiagnosticoEmpresarial().getDiagnostico().getGestionOperacional());
+            diagnostico.setGestionProductividad(process.getProcessEmpresario().getDiagnosticoEmpresarial().getDiagnostico().getGestionProductividad());
+            diagnostico.setGestionLogistica(process.getProcessEmpresario().getDiagnosticoEmpresarial().getDiagnostico().getGestionLogistica());
+            diagnostico.calcularTotales();
+            return ResponseEntity.status(HttpStatus.CREATED).body(diagnosticoRepository.save(diagnostico));
+        }
+
+        return ResponseEntity.notFound().build();
     }
-    
+
+    @PutMapping("/resultados/{id}")
+    public ResponseEntity<?>updateResultado(@RequestBody Process front, @PathVariable Long id){
+        Optional<AnalisisResultados>optional = analisisResultadosRepository.findById(id);
+       AnalisisResultados analisisResultados = new AnalisisResultados();
+       ProcessEmpresario processEmpresario = front.getProcessEmpresario();
+        if(optional.isPresent()){
+            analisisResultados.setGestionAmbiental(front.getProcessEmpresario().getDiagnosticoEmpresarial().getAnalisisResultados().getGestionAmbiental());
+            analisisResultados.setGestionCalidad(front.getProcessEmpresario().getDiagnosticoEmpresarial().getAnalisisResultados().getGestionCalidad());
+            analisisResultados.setGestionCalidad(front.getProcessEmpresario().getDiagnosticoEmpresarial().getAnalisisResultados().getGestionCalidad());
+            analisisResultados.setGestionDigital(front.getProcessEmpresario().getDiagnosticoEmpresarial().getAnalisisResultados().getGestionDigital());
+            analisisResultados.setGestionFinanciera(front.getProcessEmpresario().getDiagnosticoEmpresarial().getAnalisisResultados().getGestionFinanciera());
+            analisisResultados.setGestionEstrategica(front.getProcessEmpresario().getDiagnosticoEmpresarial().getAnalisisResultados().getGestionEstrategica());
+            analisisResultados.setGestionInnovacion(front.getProcessEmpresario().getDiagnosticoEmpresarial().getAnalisisResultados().getGestionInnovacion());
+            analisisResultados.setGestionIntelectual(front.getProcessEmpresario().getDiagnosticoEmpresarial().getAnalisisResultados().getGestionIntelectual());
+            analisisResultados.setGestionLogistica(front.getProcessEmpresario().getDiagnosticoEmpresarial().getAnalisisResultados().getGestionLogistica());
+            analisisResultados.setGestionOperacional(front.getProcessEmpresario().getDiagnosticoEmpresarial().getAnalisisResultados().getGestionOperacional());
+          
+            return ResponseEntity.status(HttpStatus.CREATED).body(analisisResultadosRepository.save(analisisResultados));
+        }
+        return ResponseEntity.notFound().build();
+    }
     @PutMapping("/economico")
     public ResponseEntity<?>updateEconomico(@RequestBody Process process){
         
@@ -65,6 +130,7 @@ public class ProcessEmpresarioController {
     @GetMapping("/{id}")
     public ResponseEntity<?>findById(@PathVariable Long id){
         Optional<ProcessEmpresario> optionalProcess = ProcessEmpresario.findById(id);
+
         if(optionalProcess.isPresent()){
             return ResponseEntity.ok().body(optionalProcess.get());
         }

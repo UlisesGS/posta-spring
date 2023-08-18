91941aeb6857fa131b7f2913851377d9278617cb
Crowly <leandro.n.nunez@gmail.com>
2023-08-17 21:59:24 -0300

    Agregando boton de activar desactivar clientes

--- CMR Posta/src/main/java/com/posta/crm/repository/ClientRepository.java
+++ CMR Posta/src/main/java/com/posta/crm/repository/ClientRepository.java
@@ -18,8 +18,11 @@ public interface ClientRepository extends JpaRepository<Client, Long>{
     public Page<Client> findByGender(@Param("gender") Gender gender, Pageable pageable);
     public Page<Client> findByType(String type, Pageable pageable);
     public Page<Client>findByActive(Boolean active, Pageable pageable);
-    @Query("SELECT a FROM Client a ORDER BY a.regdate DESC")
+    
+//    @Query("SELECT a FROM Client a ORDER BY a.regdate DESC")
+    @Query("SELECT a FROM Client a WHERE a.active = true ORDER BY a.regdate DESC")
     public Page<Client>findByRegdate(Pageable pageable);
+    
     @Query("SELECT b FROM Client b WHERE b.municipio.id=?1")
     public Page<Client>findByMunicipio(@Param("idMunicipio")Long idMunicipio, Pageable pageable);
     

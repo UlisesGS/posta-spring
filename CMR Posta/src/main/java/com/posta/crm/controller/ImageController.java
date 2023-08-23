89e52db7e1e41647c0c6169e82f7c823dba1c245
Crowly <leandro.n.nunez@gmail.com>
2023-08-22 22:45:09 -0300

    Cosas

--- CMR Posta/src/main/java/com/posta/crm/controller/ImageController.java
+++ CMR Posta/src/main/java/com/posta/crm/controller/ImageController.java
@@ -22,12 +22,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 
-
 @CrossOrigin(origins = "*")
 @RequestMapping("/image")
 @RestController
 public class ImageController {
-
 
     private static final String UPLOAD_DIR = "img/";
     //private static final String UPLOAD_DIR = System.getenv("UPLOAD_DIR");
@@ -42,8 +40,8 @@ public class ImageController {
 
         try {
             byte[] bytes = file.getBytes();
-            
-            String uniqueFileName=generateUniqueFileName(file.getOriginalFilename());
+
+            String uniqueFileName = generateUniqueFileName(file.getOriginalFilename());
             Path path = Paths.get(UPLOAD_DIR + uniqueFileName);
             Files.write(path, bytes);
             nuevoProceso.setDocumentoCompromiso(path.toString());
@@ -62,17 +60,16 @@ public class ImageController {
         if (nuevoProceso != null) {
             String rutaImagen = nuevoProceso.getDocumentoCompromiso();
             Path path = Paths.get(rutaImagen);
-                byte[] fileData = Files.readAllBytes(path);
-                String contentType = detectContentType(rutaImagen);
-                
-                HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.parseMediaType(contentType));
-        headers.setContentDisposition(ContentDisposition.builder("inline").filename(path.getFileName().toString()).build());
+            byte[] fileData = Files.readAllBytes(path);
+            String contentType = detectContentType(rutaImagen);
+
+            HttpHeaders headers = new HttpHeaders();
+            headers.setContentType(MediaType.parseMediaType(contentType));
+            headers.setContentDisposition(ContentDisposition.builder("inline").filename(path.getFileName().toString()).build());
             return new ResponseEntity<>(fileData, headers, HttpStatus.OK);
         }
         return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
-    
 
     @PostMapping("/uploadEncuesta/{id}")
     public String uploadImage1(@RequestParam("file") MultipartFile file, @PathVariable Long id) {
@@ -92,6 +89,38 @@ public class ImageController {
             return "Error al subir la imagen";
         }
     }
+//    @PostMapping("/uploadEncuesta/{id}")
+//    public String uploadImage1(@RequestParam("file") MultipartFile file, @PathVariable Long id) {
+//        Process nuevoProceso = processServiceImpl.findById(id).orElse(null); // Evitar nullPointerException si no se encuentra el proceso
+//
+//        if (nuevoProceso == null) {
+//            return "Proceso no encontrado";
+//        }
+//
+//        try {
+//            byte[] bytes = file.getBytes();
+//            String originalFilename = file.getOriginalFilename();
+//            String fileExtension = originalFilename.substring(originalFilename.lastIndexOf(".") + 1);
+//
+//            // Comprueba la extensión del archivo (puedes agregar más extensiones si es necesario)
+//            if ("jpg".equalsIgnoreCase(fileExtension) || "jpeg".equalsIgnoreCase(fileExtension)
+//                    || "png".equalsIgnoreCase(fileExtension) || "gif".equalsIgnoreCase(fileExtension)
+//                    || "pdf".equalsIgnoreCase(fileExtension)) {
+//
+//                String uniqueFileName = generateUniqueFileName(originalFilename);
+//                Path path = Paths.get(UPLOAD_DIR + uniqueFileName);
+//                Files.write(path, bytes);
+//                nuevoProceso.setImpacto(path.toString());
+//                processServiceImpl.save(nuevoProceso);
+//                return "Archivo subido exitosamente!";
+//            } else {
+//                return "Tipo de archivo no admitido. Se admiten archivos JPG, JPEG, PNG, GIF y PDF.";
+//            }
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//            return "Error al subir el archivo";
+//        }
+//    }
 
     @GetMapping(value = "/imagenEncuesta/{id}", produces = MediaType.IMAGE_JPEG_VALUE)
     public ResponseEntity<byte[]> getImage1(@PathVariable Long id) throws IOException {
@@ -100,12 +129,12 @@ public class ImageController {
         if (nuevoProceso != null) {
             String rutaImagen = nuevoProceso.getEncuestaSatisfaccion();
             Path path = Paths.get(rutaImagen);
-                byte[] fileData = Files.readAllBytes(path);
-                String contentType = detectContentType(rutaImagen);
-                
-                HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.parseMediaType(contentType));
-        headers.setContentDisposition(ContentDisposition.builder("inline").filename(path.getFileName().toString()).build());
+            byte[] fileData = Files.readAllBytes(path);
+            String contentType = detectContentType(rutaImagen);
+
+            HttpHeaders headers = new HttpHeaders();
+            headers.setContentType(MediaType.parseMediaType(contentType));
+            headers.setContentDisposition(ContentDisposition.builder("inline").filename(path.getFileName().toString()).build());
             return new ResponseEntity<>(fileData, headers, HttpStatus.OK);
         }
         return new ResponseEntity<>(HttpStatus.NOT_FOUND);
@@ -118,7 +147,7 @@ public class ImageController {
 
         try {
             byte[] bytes = file.getBytes();
-           String uniqueFileName=generateUniqueFileName(file.getOriginalFilename());
+            String uniqueFileName = generateUniqueFileName(file.getOriginalFilename());
             Path path = Paths.get(UPLOAD_DIR + uniqueFileName);
             Files.write(path, bytes);
             nuevoProceso.setActaCierre(path.toString());
@@ -137,17 +166,17 @@ public class ImageController {
         if (nuevoProceso != null) {
             String rutaImagen = nuevoProceso.getActaCierre();
             Path path = Paths.get(rutaImagen);
-                byte[] fileData = Files.readAllBytes(path);
-                String contentType = detectContentType(rutaImagen);
-                
-                HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.parseMediaType(contentType));
-        headers.setContentDisposition(ContentDisposition.builder("inline").filename(path.getFileName().toString()).build());
+            byte[] fileData = Files.readAllBytes(path);
+            String contentType = detectContentType(rutaImagen);
+
+            HttpHeaders headers = new HttpHeaders();
+            headers.setContentType(MediaType.parseMediaType(contentType));
+            headers.setContentDisposition(ContentDisposition.builder("inline").filename(path.getFileName().toString()).build());
             return new ResponseEntity<>(fileData, headers, HttpStatus.OK);
         }
         return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
-    
+
     @GetMapping(value = "/imagenImpacto/{id}", produces = MediaType.IMAGE_JPEG_VALUE)
     public ResponseEntity<byte[]> getImage3(@PathVariable Long id) throws IOException {
         Process nuevoProceso = processServiceImpl.findById(id).get();
@@ -155,12 +184,12 @@ public class ImageController {
         if (nuevoProceso != null) {
             String rutaImagen = nuevoProceso.getImpacto();
             Path path = Paths.get(rutaImagen);
-                byte[] fileData = Files.readAllBytes(path);
-                String contentType = detectContentType(rutaImagen);
-                
-                HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.parseMediaType(contentType));
-        headers.setContentDisposition(ContentDisposition.builder("inline").filename(path.getFileName().toString()).build());
+            byte[] fileData = Files.readAllBytes(path);
+            String contentType = detectContentType(rutaImagen);
+
+            HttpHeaders headers = new HttpHeaders();
+            headers.setContentType(MediaType.parseMediaType(contentType));
+            headers.setContentDisposition(ContentDisposition.builder("inline").filename(path.getFileName().toString()).build());
             return new ResponseEntity<>(fileData, headers, HttpStatus.OK);
         }
         return new ResponseEntity<>(HttpStatus.NOT_FOUND);
@@ -173,7 +202,7 @@ public class ImageController {
 
         try {
             byte[] bytes = file.getBytes();
-           String uniqueFileName=generateUniqueFileName(file.getOriginalFilename());
+            String uniqueFileName = generateUniqueFileName(file.getOriginalFilename());
             Path path = Paths.get(UPLOAD_DIR + uniqueFileName);
             Files.write(path, bytes);
             nuevoProceso.setImpacto(path.toString());
@@ -184,10 +213,7 @@ public class ImageController {
             return "Error al subir la imagen";
         }
     }
-    
-    
-    
-    
+
     //Metodo para modificar el nombre del archivo
     private String generateUniqueFileName(String originalFileName) {
         String extension = "";
@@ -198,26 +224,25 @@ public class ImageController {
         String uniquePart = Instant.now().toEpochMilli() + "-" + UUID.randomUUID().toString();
         return uniquePart + extension;
     }
-    
-    private String detectContentType(String fileName) {
-    String extension = FilenameUtils.getExtension(fileName);
-    
-    // Agrega lógica para detectar otros tipos de archivos según su extensión, por ejemplo, PDF, DOCX, etc.
-    if ("pdf".equalsIgnoreCase(extension)) {
-    return MediaType.APPLICATION_PDF_VALUE;
-} else if ("docx".equalsIgnoreCase(extension)) {
-    return "application/vnd.openxmlformats-officedocument.wordprocessingml.document";
-} else if ("jpg".equalsIgnoreCase(extension) || "jpeg".equalsIgnoreCase(extension)) {
-    return MediaType.IMAGE_JPEG_VALUE;
-} else if ("png".equalsIgnoreCase(extension)) {
-    return MediaType.IMAGE_PNG_VALUE;
-} else if ("gif".equalsIgnoreCase(extension)) {
-    return MediaType.IMAGE_GIF_VALUE;
-}
-    
-    // Si no se reconoce la extensión, puedes devolver un tipo de contenido genérico
-    return MediaType.APPLICATION_OCTET_STREAM_VALUE;
-}
 
+    private String detectContentType(String fileName) {
+        String extension = FilenameUtils.getExtension(fileName);
+
+        // Agrega lógica para detectar otros tipos de archivos según su extensión, por ejemplo, PDF, DOCX, etc.
+        if ("pdf".equalsIgnoreCase(extension)) {
+            return MediaType.APPLICATION_PDF_VALUE;
+        } else if ("docx".equalsIgnoreCase(extension)) {
+            return "application/vnd.openxmlformats-officedocument.wordprocessingml.document";
+        } else if ("jpg".equalsIgnoreCase(extension) || "jpeg".equalsIgnoreCase(extension)) {
+            return MediaType.IMAGE_JPEG_VALUE;
+        } else if ("png".equalsIgnoreCase(extension)) {
+            return MediaType.IMAGE_PNG_VALUE;
+        } else if ("gif".equalsIgnoreCase(extension)) {
+            return MediaType.IMAGE_GIF_VALUE;
+        }
+
+        // Si no se reconoce la extensión, puedes devolver un tipo de contenido genérico
+        return MediaType.APPLICATION_OCTET_STREAM_VALUE;
+    }
 
 }
